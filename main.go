cd498c0384610a6fbc5004e43b2334abd6dea5c3
kshitij <kshitijhack@gmail.com>
2020-05-25 14:43:27 +0530

    created api end point

--- main.go
+++ main.go
@@ -25,6 +25,24 @@ var events = allEvents{
 	},
 }
 
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
+
+
+
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
