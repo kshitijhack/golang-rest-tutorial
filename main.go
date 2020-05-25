d426f1d41f554739d7bb561f9ab90a9f57bd931f
kshitij <kshitijhack@gmail.com>
2020-05-25 14:42:00 +0530

    struct added

--- main.go
+++ main.go
@@ -8,6 +8,23 @@ import (
 	"github.com/gorilla/mux"
 )
 
+
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+}
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
